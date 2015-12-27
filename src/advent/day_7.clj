7874bfa3fb2606dd2ad2127bb194cf07ab6965c6
Geoff Shannon <geoffpshannon@gmail.com>
2015-12-27 04:32:45 -0600

    Finish Day 7

--- src/advent/day_7.clj
+++ src/advent/day_7.clj
@@ -1,8 +1,11 @@
 (ns advent.day-7
-  (:require [clojure.repl :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.repl :refer :all]
             [clojure.string :as str]
             [instaparse.core :as insta]
-            [loom.graph :as graph]))
+            [loom.alg :as alg]
+            [loom.graph :as graph]
+            [advent.core :refer [slurp-resource]]))
 
 (def raw-parse-wires
   (insta/parser
@@ -37,17 +40,93 @@
                          :id symbol
                          :num-lit #(Integer/parseInt %)})))
 
-(defn symbols [data]
+(defn symbols [& data]
   (filter symbol? (flatten data)))
 
 (defn wire->dependencies [[wire-name input]]
   (for [sym (symbols input)]
-    [wire-name sym]))
+    [sym wire-name]))
 
 (defn circuit->wire-dependencies [circuit]
   (->> circuit
        (mapcat wire->dependencies)
        (apply graph/digraph)))
 
+(defmulti propagate-input
+  (fn [resolved-circuit input]
+    (cond
+      (or (number? input)
+          (symbol? input)) :number-or-symbol
+
+      (vector? input)
+      (let [[op-key] input] op-key)
+      :else :default))
+  :default :default)
+
+(defn lookup-wire-or-value [circuit wire-or-value]
+  (get circuit wire-or-value wire-or-value))
+
+(defmethod propagate-input :default [circuit input]
+  (throw (ex-info (format "Unrecognized operation '%s' requested." input)
+                  {:input input :circuit circuit})))
+
+(defmethod propagate-input :number-or-symbol
+  [circuit value]
+  (lookup-wire-or-value circuit value))
+
+(defmethod propagate-input :not [circuit [_ value]]
+  (bit-not (lookup-wire-or-value circuit value)))
+
+(defmacro defbinary-circuit-op [op-name fn-op]
+  `(defmethod propagate-input ~(keyword op-name)
+     [circuit# [_ left# right#]]
+     (~fn-op
+      (lookup-wire-or-value circuit# left#)
+      (lookup-wire-or-value circuit# right#))))
+
+(defbinary-circuit-op and bit-and)
+(defbinary-circuit-op or bit-or)
+(defbinary-circuit-op lshift bit-shift-left)
+(defbinary-circuit-op rshift unsigned-bit-shift-right)
+
+(defn bigint? [value]
+  (= clojure.lang.BigInt
+     (class value)))
+
+(defn as-unsigned-short [value]
+  (bit-and 0xffff (int value)))
+
+(defn resolve-wire [circuit wire-name]
+  (assoc circuit
+         wire-name
+         (as-unsigned-short
+          (propagate-input circuit
+                           (get circuit wire-name)))))
+
 (defn resolve-circuit [circuit]
-  )
+  (let [dependencies (circuit->wire-dependencies circuit)]
+    (loop [result circuit
+           wire-names (alg/topsort dependencies)]
+      (if-let [current-wire (first wire-names)]
+        (recur (resolve-wire result current-wire)
+               (rest wire-names))
+        result))))
+
+(defn read-circuit [file-name]
+  (-> file-name
+      slurp-resource
+      parse-wires))
+
+(defn day-7 [file-name]
+  (-> file-name
+      read-circuit
+      resolve-circuit
+      (get 'a)))
+
+(defn day-7-2 [file-name]
+  (let [a-value (day-7 file-name)]
+    (-> file-name
+        read-circuit
+        (assoc 'b a-value)
+        resolve-circuit
+        (get 'a))))
--- test/advent/day_7_test.clj
+++ test/advent/day_7_test.clj
@@ -1,6 +1,7 @@
 (ns advent.day-7-test
   (:require [advent.day-7 :as sut]
             [clojure.test :as t]
+            [loom.alg :as alg]
             [loom.graph :as graph]))
 
 (t/deftest parse-wires-test
@@ -24,6 +25,8 @@
 
 (t/deftest symbols-test
   (t/is (= '[a]
+           (sut/symbols 'a)))
+  (t/is (= '[a]
            (sut/symbols '[a])))
   (t/is (= '[a b]
            (sut/symbols '[a b])))
@@ -37,23 +40,67 @@
 (t/deftest wire->dependencies-test
   (t/is (= []
            (sut/wire->dependencies '[x 123])))
-  (t/is (= '[[y x]]
+  (t/is (= '[[x y]]
            (sut/wire->dependencies '[y [:not x]])))
-  (t/is (= '#{[z x]
-              [z y]}
+  (t/is (= '#{[x z]
+              [y z]}
            (set
             (sut/wire->dependencies '[z [:and x y]])))))
 
 (t/deftest circuit->wire-dependencies-test
-  (t/is (= (graph/digraph '[y x])
-           (sut/circuit->wire-dependencies {'x 123 'y [:not 'x]})))
-  (t/is (= (graph/digraph '[y x] '[z y] '[z x])
-           (sut/circuit->wire-dependencies {'x 123
-                                            'y [:not 'x]
-                                            'z [:or 'x 'y]}))))
+  (let [circuit-deps (sut/circuit->wire-dependencies {'x 123 'y [:not 'x]})]
+    (t/is (= (graph/digraph '[x y])
+             circuit-deps))
+    (t/is (= '(x y)
+             (alg/topsort circuit-deps))))
+
+  (let [circuit-deps (sut/circuit->wire-dependencies {'x 123
+                                                      'y [:not 'x]
+                                                      'z [:or 'x 'y]})]
+    (t/is (= (graph/digraph '[x y] '[y z] '[x z])
+             circuit-deps))
+    (t/is (= '(x y z)
+             (alg/topsort circuit-deps)))))
+
+(t/deftest as-unsigned-short-test
+  (t/is (= 124
+           (sut/as-unsigned-short (bigint 124))))
+  (t/is (= 65412
+           (sut/as-unsigned-short (bigint -124)))))
+
+(t/deftest resolve-wire-test
+  (t/is (= '{x 123 y [:not x]}
+           (sut/resolve-wire '{x 123 y [:not x]} 'x)))
+  (t/is (= '{x 123 y 65412}
+           (sut/resolve-wire '{x 123 y [:not x]} 'y)))
+  (t/is (= '{x 123 y 3}
+           (sut/resolve-wire '{x 123 y [:and 7 x]} 'y)))
+  (t/is (= '{x 123 y 127}
+           (sut/resolve-wire '{x 123 y [:or 4 x]} 'y)))
+  (t/is (= '{x 123 y 492}
+           (sut/resolve-wire '{x 123 y [:lshift x 2]} 'y)))
+  (t/is (= '{x 123 y 30}
+           (sut/resolve-wire '{x 123 y [:rshift x 2]} 'y)))
+
+  (t/is (= '{x 123 y [:not x] f 492}
+           (sut/resolve-wire '{x 123
+                               y [:not x]
+                               f [:lshift x 2]}
+                             'f))))
 
 (t/deftest resolve-circuit-test
-  #_(t/is (= '{d 72     ;; d: 72
+  (t/is (= '{x 123
+             y 65412}
+           (sut/resolve-circuit '{x 123 y [:not x]})))
+  (t/is (= '{y 123
+             x 65412}
+           (sut/resolve-circuit '{y 123 x [:not y]})))
+  (t/is (= '{z 123
+             y 123
+             x 65412}
+           (sut/resolve-circuit '{z y y 123 x [:not y]})))
+
+  (t/is (= '{d 72     ;; d: 72
              e 507    ;; e: 507
              f 492    ;; f: 492
              g 114    ;; g: 114
@@ -68,6 +115,4 @@
                                   f [:lshift x 2]  ;; x LSHIFT 2 -> f
                                   g [:rshift y 2]  ;; y RSHIFT 2 -> g
                                   h [:not x]       ;; NOT x -> h
-                                  i [:not y]})))   ;; NOT y -> i
-
-  )
+                                  i [:not y]}))))  ;; NOT y -> i
