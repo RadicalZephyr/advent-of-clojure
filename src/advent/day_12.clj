cada029d742e2f3146ab30660e15a2a660c35683
Geoff Shannon <geoffpshannon@gmail.com>
2016-09-04 12:48:26 -0500

    Day 12 part 1

--- src/advent/day_12.clj
+++ src/advent/day_12.clj
@@ -1,1 +1,21 @@
-(ns advent.day-12)
+(ns advent.day-12
+  (:require [instaparse.core :as insta]
+            [advent.core :refer [slurp-resource]]))
+
+(def raw-parse-ints
+  (insta/parser
+   "<json-numbers> = junk? ( number junk )* number? junk?
+    number = #'-?[0-9]+'
+    <junk> = <#'[^-0-9]+'>"))
+
+(defn parse-ints [s]
+  (->> (raw-parse-ints s)
+       (insta/transform {:number #(Integer/parseInt %)})))
+
+(defn sum-numbers [s]
+  (apply + (parse-ints s)))
+
+(defn day-12 [file]
+  (->> file
+       slurp-resource
+       sum-numbers))
--- test/advent/day_12_test.clj
+++ test/advent/day_12_test.clj
@@ -1,3 +1,45 @@
 (ns advent.day-12-test
   (:require [advent.day-12 :as sut]
             [clojure.test :as t]))
+
+
+(t/deftest parse-ints-test
+  (t/is (= [1]
+           (sut/parse-ints "1")))
+  (t/is (= [-1]
+           (sut/parse-ints "-1")))
+  (t/is (= [1 2]
+           (sut/parse-ints "1a2")))
+  (t/is (= [1 2]
+           (sut/parse-ints "1a2b")))
+  (t/is (= [1 2]
+           (sut/parse-ints "aoeu1a2b")))
+  (t/is (= [1 2]
+           (sut/parse-ints "1a,^&^%^(&*)2b")))
+  (t/is (= [1 2]
+           (sut/parse-ints "1a2baoseunthoa%^&GDIDHT")))
+
+  (t/is (= [-1 1]
+           (sut/parse-ints "{\"a\":[-1,1]}"))))
+
+(t/deftest sum-numbers-test
+  (t/is (= 6
+           (sut/sum-numbers "[1,2,3]")))
+  (t/is (= 6
+           (sut/sum-numbers "{\"a\":2,\"b\":4}")))
+  (t/is (= 3
+           (sut/sum-numbers "[[[3]]]")))
+  (t/is (= 3
+           (sut/sum-numbers "{\"a\":{\"b\":4},\"c\":-1}")))
+  (t/is (= 0
+         (sut/sum-numbers "{\"a\":[-1,1]}")))
+  (t/is (= 0
+           (sut/sum-numbers "[-1,{\"a\":1}]")))
+  (t/is (= 0
+         (sut/sum-numbers "[]")))
+  (t/is (= 0
+         (sut/sum-numbers "{}")))
+
+
+
+  )
