df788c4acd2298cd40bfc84adf595128c0387f9b
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-03 21:11:20 -0600

    Upgrade some dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject advent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Advent of Code 2015 puzzle solutions"
+  :url "https://github.com/RadicalZephyr/advent-of-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.3"]
-                 [instaparse "1.4.1"]
-                 [aysylu/loom "0.5.4"]
+                 [instaparse "1.4.3"]
+                 [aysylu/loom "0.6.0"]
                  [cheshire "5.6.3"]])
