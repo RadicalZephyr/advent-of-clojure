c81b33c84fb9840a6ca14239ba6880ab4bf362f8
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-16 23:49:28 -0700

    Change parsed data format

--- src/advent/day_15.clj
+++ src/advent/day_15.clj
@@ -12,15 +12,14 @@
     attr-name = #'[a-z]+'
     attr-value = #'-?[0-9]+'"))
 
-(defn- into-map [& pairs]
-  (into {} pairs))
+
 
 (defn parse-ingredients [ingredients-list]
   (->> ingredients-list
        raw-parse-ingredients
-       (insta/transform {:ingredients into-map
-                         :ingredient vector
-                         :attrs into-map
+       (insta/transform {:ingredients (fn into-set [& maps] (into #{} maps))
+                         :ingredient #(assoc %2 :name %1)
+                         :attrs (fn into-map [& pairs] (into {} pairs))
                          :attr vector
                          :attr-name keyword
                          :attr-value #(Integer/parseInt %)})))
--- test/advent/day_15_test.clj
+++ test/advent/day_15_test.clj
@@ -3,23 +3,26 @@
             [clojure.test :as t]))
 
 (t/deftest parse-ingredients-test
-  (t/is (= {"Butterscotch" {:capacity -1
-                            :durability -2
-                            :flavor 6
-                            :texture 3
-                            :calories 8}}
+  (t/is (= #{{:name "Butterscotch"
+               :capacity -1
+               :durability -2
+               :flavor 6
+               :texture 3
+               :calories 8}}
            (sut/parse-ingredients "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8")))
 
-  (t/is (= {"Butterscotch" {:capacity -1
-                            :durability -2
-                            :flavor 6
-                            :texture 3
-                            :calories 8}
-            "Cinnamon" {:capacity 2
-                        :durability 3
-                        :flavor -2
-                        :texture -1
-                        :calories 3}}
+  (t/is (= #{{:name "Butterscotch"
+              :capacity -1
+              :durability -2
+              :flavor 6
+              :texture 3
+              :calories 8}
+             {:name "Cinnamon"
+              :capacity 2
+              :durability 3
+              :flavor -2
+              :texture -1
+              :calories 3}}
            (sut/parse-ingredients
             (str "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\n"
                  "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3\n")))))
