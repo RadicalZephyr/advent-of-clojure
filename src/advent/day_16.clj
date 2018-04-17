5e9a653473e9228d3fe43ae2e9e0a1c8cb161947
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-17 03:08:37 -0700

    Finish day 16

--- src/advent/day_16.clj
+++ src/advent/day_16.clj
@@ -1,5 +1,6 @@
 (ns advent.day-16
-  (:require [instaparse.core :as insta]))
+  (:require [advent.core :refer [slurp-resource]]
+            [instaparse.core :as insta]))
 
 (def raw-parse-aunts
   (insta/parser
@@ -22,3 +23,55 @@
                          :attr vector
                          :attr-name keyword
                          :number #(Integer/parseInt %)})))
+
+(defn aunt-matches? [clues aunt]
+  (= (select-keys clues (keys aunt)) aunt))
+
+(def clues
+  {:children 3
+   :cats 7
+   :samoyeds 2
+   :pomeranians 3
+   :akitas 0
+   :vizslas 0
+   :goldfish 5
+   :trees 3
+   :cars 2
+   :perfumes 1})
+
+(defn day-16 [file-name]
+  (->> file-name
+       slurp-resource
+       parse-aunts
+       (some #(when (aunt-matches? clues (second %))
+                (first %)))))
+
+(defn map-cmp [f m1 m2]
+  (every? #(f (% m1) (% m2)) (keys m1)))
+
+(defn map< [m1 m2]
+  (map-cmp < m1 m2))
+
+(defn map> [m1 m2]
+  (map-cmp > m1 m2))
+
+(defn split-auntributes [attrs]
+  [(select-keys attrs [:cats :trees])
+   (dissoc attrs :cats :trees :pomeranions :goldfish)
+   (select-keys attrs [:pomeranions :goldfish])])
+
+(defn aunt-matches2? [clues aunt]
+  (let [relevant-clues (select-keys clues (keys aunt))]
+    (every? identity
+     (map apply
+          [map< = map>]
+          (split-auntributes relevant-clues)
+          (split-auntributes aunt)
+          [[] [] []]))))
+
+(defn day-16-2 [file-name]
+  (->> file-name
+       slurp-resource
+       parse-aunts
+       (some #(when (aunt-matches2? clues (second %))
+                (first %)))))
--- test/advent/day_16_test.clj
+++ test/advent/day_16_test.clj
@@ -7,3 +7,38 @@
            (sut/parse-aunts "Sue 1: trees: 1, goldfish: 2, cats: 0")))
   (t/is (= {100 {:fowl 1 :blue 2 :cats 0}}
            (sut/parse-aunts "Sue 100: fowl: 1, blue: 2, cats: 0"))))
+
+(t/deftest aunt-matches?-test
+  (t/is (= true
+           (sut/aunt-matches? {:a 1}
+                              {:a 1})))
+  (t/is (= true
+           (sut/aunt-matches? {:a 1 :b 2}
+                              {:a 1}))))
+
+(t/deftest map<-test
+  (t/is (= true
+           (sut/map< {:a 1}
+                     {:a 2})))
+  (t/is (= false
+           (sut/map< {:a 3}
+                     {:a 2}))))
+
+(t/deftest map>-test
+  (t/is (= false
+           (sut/map> {:a 1}
+                     {:a 2})))
+  (t/is (= true
+           (sut/map> {:a 3}
+                     {:a 2}))))
+
+(t/deftest aunt-matches2?-test
+  (t/is (= true
+           (sut/aunt-matches2? {:a 1}
+                               {:a 1})))
+  (t/is (= true
+           (sut/aunt-matches2? {:cats 1 :a 1}
+                               {:cats 2})))
+  (t/is (= true
+           (sut/aunt-matches2? {:cats 1 :goldfish 3}
+                               {:cats 2 :goldfish 1}))))
