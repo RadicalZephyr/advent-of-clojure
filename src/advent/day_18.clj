1837c11cd956d33bcac5f25aa95a07199d4b43e6
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-17 23:24:26 -0700

    Finish day 18 part 1

--- src/advent/day_18.clj
+++ src/advent/day_18.clj
@@ -14,3 +14,48 @@
        (insta/transform {:grid vector
                          :line vector
                          :space keyword})))
+
+(defn neighbors [[max-x max-y] [x y]]
+  (for [dx (range -1 2)
+        dy (range -1 2)
+        :let [nx (+ x dx)
+              ny (+ y dy)]
+        :when (and
+               (not= 0 dx dy)
+               (>= max-x nx 0)
+               (>= max-y ny 0))]
+    [nx ny]))
+
+(defn step-board [size grid]
+  (let [size (mapv dec size)]
+    (->> grid
+         (mapcat #(neighbors size %))
+         frequencies
+         (keep (fn [[coord count]]
+                 (if (contains? grid coord)
+                   (when (> 4 count 1) coord)
+                   (when (= 3 count) coord))))
+         set)))
+
+(defn dense->sparse [dense-grid]
+  (->> dense-grid
+       (keep-indexed
+        (fn [x row]
+          (seq
+           (keep-indexed (fn [y space]
+                           (when (= :# space)
+                             [x y]))
+                         row))))
+       (apply concat)
+       set))
+
+(defn day-18 [file-name]
+  (let [grid (->> file-name
+                  slurp-resource
+                  parse-grid
+                  dense->sparse)]
+    (count
+     (reduce (fn [grid _]
+               (step-board [100 100] grid))
+             grid
+             (range 100)))))
--- test/advent/day_18_test.clj
+++ test/advent/day_18_test.clj
@@ -12,3 +12,42 @@
             [:. :# :.]
             [:# :. :.]]
            (sut/parse-grid "..#\n.#.\n#..\n"))))
+
+
+(t/deftest neighbors-test
+  (t/is (= [[0 1] [1 0] [1 1]]
+           (sut/neighbors [2 2] [0 0]))))
+
+(t/deftest step-board-test
+  (t/is (= #{[0 0] [0 1] [1 0] [1 1]}
+           (sut/step-board
+            [2 2]
+            #{[0 1] [1 0] [1 1]})))
+
+  (t/is (= #{}
+           (sut/step-board
+            [2 2]
+            #{[0 0] [0 1]})))
+
+  (t/is (= #{[1 1] [1 2]
+             [2 1] [2 2]}
+           (sut/step-board
+            [4 4]
+            #{[1 1] [1 2]
+              [2 1] [2 2]})))
+
+  (t/is (= #{[0 0] [0 1] [0 2]
+             [1 0]       [1 2]
+             [2 0] [2 1] [2 2]}
+           (sut/step-board
+            [3 3]
+            #{[0 1] [1 0] [1 1]
+              [2 1] [1 2]}))))
+
+(t/deftest dense->sparse-test
+  (t/is (= #{[0 0] [0 1]}
+           (sut/dense->sparse [[:# :#]
+                               [:. :.]])))
+  (t/is (= #{[1 0] [1 1]}
+           (sut/dense->sparse [[:. :.]
+                               [:# :#]]))))
