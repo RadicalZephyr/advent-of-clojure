887533b08b8a5a254900a9e9ede94b77ab9c079a
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-18 06:00:44 -0700

    Try some other things

--- src/advent/day_20.clj
+++ src/advent/day_20.clj
@@ -5,12 +5,28 @@
        (keep #(when (= 0 (mod x %)) %))
        (apply + 1 x)))
 
-(defn presents-seq []
-  (->> (range)
-       (map (comp factorsum inc))))
+(defn presents-seq
+  ([]
+   (presents-seq 1))
+  ([start]
+   (->> (range)
+        (map (comp factorsum #(+ start %))))))
 
-(defn day-20 [puzzle-num]
+(defn search-factors [puzzle-num]
   (let [puzzle-name (/ puzzle-num 10)]
     (->> (presents-seq)
          (drop-while #(> puzzle-num %))
          first)))
+
+(defn factorial-seq
+  ([]
+   (factorial-seq 1 2))
+  ([x i]
+   (lazy-seq
+    (cons x
+          (factorial-seq (* x i) (inc i))))))
+
+(defn partitions [x]
+  (set
+   (lazy-cat
+    [(repeat x 1) [x]])))
--- test/advent/day_20_test.clj
+++ test/advent/day_20_test.clj
@@ -2,7 +2,7 @@
   (:require [advent.day-20 :as sut]
             [clojure.test :as t]))
 
-(t/deftest factors-test
+(t/deftest factorsum-test
   (t/is (= 3
            (sut/factorsum 2)))
 
@@ -11,3 +11,23 @@
 
   (t/is (= 12
            (sut/factorsum 6))))
+
+(t/deftest factorial-seq-test
+  (t/is (= 1
+           (nth (sut/factorial-seq) 0)))
+  (t/is (= 2
+           (nth (sut/factorial-seq) 1)))
+  (t/is (= 6
+           (nth (sut/factorial-seq) 2)))
+  (t/is (= 24
+           (nth (sut/factorial-seq) 3)))
+  (t/is (= 120
+           (nth (sut/factorial-seq) 4))))
+
+(t/deftest partitions-test
+  (t/is (= #{[1]}
+           (sut/partitions 1)))
+  (t/is (= #{[1 1] [2]}
+           (sut/partitions 2)))
+  (t/is (= #{[1 1 1] [1 2] [3]}
+           (sut/partitions 3))))
