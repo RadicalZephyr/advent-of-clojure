ed12051c8a72c8d6aa1cbd655024150a2326f11e
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-19 03:57:28 -0700

    Complete day 20 part 1
    Jeez!!! Only took like 10 hours...

--- src/advent/day_20.clj
+++ src/advent/day_20.clj
@@ -1,4 +1,6 @@
-(ns advent.day-20)
+(ns advent.day-20
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.math.numeric-tower :as math]))
 
 (defn factorsum [x]
   (->> (range 2 (inc (quot x 2)))
@@ -26,7 +28,78 @@
     (cons x
           (factorial-seq (* x i) (inc i))))))
 
+(defn prime-seq
+  ([]
+   (prime-seq (drop 2 (range))))
+  ([ps]
+   (lazy-seq
+    (let [p (first ps)]
+      (cons p
+            (prime-seq (remove #(= 0 (mod % p)) (rest ps))))))))
+
+(def primes-1000
+  (vec (take 1000 (prime-seq))))
+
 (defn partitions [x]
-  (set
-   (lazy-cat
-    [(repeat x 1) [x]])))
+  (->> (repeat x 1)
+       (combo/partitions)
+       (map (fn [xs] (map #(apply + %) xs)))))
+
+(defn make-factorization [primes exps]
+  (try
+    (apply *
+           (map math/expt primes exps))
+    (catch ArithmeticException e
+      nil)))
+
+(defn highly-factorable-seq [x]
+  (let [primes (prime-seq)
+        partitions (partitions x)]
+    (mapcat #(map math/expt primes %) partitions)))
+
+(defn prime-exps-for [x n]
+  (loop [f x
+         i 0]
+    (if (> f 1)
+      (recur (quot f n)
+             (inc i))
+      i)))
+
+(defn prime-exps-seq
+  ([x]
+   (prime-exps-seq x (prime-seq)))
+  ([x primes]
+   (lazy-seq
+    (cons (prime-exps-for x (first primes))
+          (prime-exps-seq x (rest  primes))))))
+
+(defn prime!-seq []
+  (reductions * (prime-seq)))
+
+(defmacro prime-factors [prime-limit]
+  (let [primes (prime-seq)
+        prime-exp-limits (->> primes
+                              (map (juxt #(gensym (str "i" % "-"))
+                                         identity
+                                         #(prime-exps-for 1500000 %)))
+                              (take prime-limit)
+                              vec
+                              reverse)]
+    `(for [~@(mapcat (fn [[sym _ limit]]
+                       (vector sym `(range ~limit)))
+                     prime-exp-limits)]
+       (try
+         (* ~@(map (fn [[sym p _]]
+                     `(math/expt ~p ~sym))
+                   prime-exp-limits))
+         (catch ArithmeticException e#
+           nil)))))
+
+(defn day-20 [x]
+  (let [primes (prime-seq)]
+    (->> (range 20)
+         (drop 1)
+         (mapcat partitions)
+         (keep #(make-factorization primes %))
+         sort
+         (some #(when (>= (factorsum %) x) %)))))
--- test/advent/day_20_test.clj
+++ test/advent/day_20_test.clj
@@ -10,7 +10,16 @@
            (sut/factorsum 4)))
 
   (t/is (= 12
-           (sut/factorsum 6))))
+           (sut/factorsum 6)))
+
+  (t/is (= 8
+           (sut/factorsum 7)))
+
+  (t/is (= 15
+           (sut/factorsum 8)))
+
+  (t/is (= 13
+           (sut/factorsum 9))))
 
 (t/deftest factorial-seq-test
   (t/is (= 1
@@ -24,10 +33,22 @@
   (t/is (= 120
            (nth (sut/factorial-seq) 4))))
 
+(t/deftest prime-seq-test
+  (t/is (= [2]
+           (take 1 (sut/prime-seq))))
+  (t/is (= [2 3]
+           (take 2 (sut/prime-seq))))
+  (t/is (= [2 3 5]
+           (take 3 (sut/prime-seq))))
+  (t/is (= [2 3 5 7]
+           (take 4 (sut/prime-seq)))))
+
 (t/deftest partitions-test
-  (t/is (= #{[1]}
+  (t/is (= [[1]]
            (sut/partitions 1)))
-  (t/is (= #{[1 1] [2]}
+  (t/is (= [[2] [1 1]]
            (sut/partitions 2)))
-  (t/is (= #{[1 1 1] [1 2] [3]}
-           (sut/partitions 3))))
+  (t/is (= [[3] [2 1] [1 1 1]]
+           (sut/partitions 3)))
+  (t/is (= [[4] [3 1] [2 2] [2 1 1] [1 1 1 1]]
+           (sut/partitions 4))))
